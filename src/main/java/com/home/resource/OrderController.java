b26d4f11dd7279600544079d7a8bd0b721645f0e
rahulbaliyan404@gmail.com <rahulbaliyan404@gmail.com>
2026-01-26 02:00:15 +0530

    renamed controller

--- src/main/java/com/home/resource/OrderController.java
+++ src/main/java/com/home/resource/OrderController.java
@@ -1,9 +1,6 @@
 package com.home.resource;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.home.dto.User;
 import com.home.service.OrderService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/orders")
+@RequestMapping("/api")
 public class OrderController {
 
     private final OrderService orderService;
@@ -20,7 +17,7 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-   @GetMapping("/{id}")
+   @GetMapping("/order/{id}")
    public ResponseEntity<User> getOrder(@PathVariable Long id) {
        User result = orderService.placeOrder(id);
        if (result == null) {
